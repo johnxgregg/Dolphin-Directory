b735fce5036b1a41ed2cee4547af53f51fb8bcc4
John Gregg <johnxgregg@icloud.com>
2022-06-01 21:50:56 -0700

    project template setup

---
+++ flask_app/controllers/contacts.py
@@ -0,0 +1,7 @@
+from flask import render_template,redirect,session,request,flash
+from flask_app import app
+from flask_app.models.contact import Contact
+from flask_app.models.user import User
+
+
+#ROUTES
