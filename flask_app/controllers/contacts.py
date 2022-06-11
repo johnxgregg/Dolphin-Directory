5a6f997d3218f11b90000957a2b06853b670c8b8
John Gregg <johnxgregg@icloud.com>
2022-06-11 13:44:24 -0700

    now functional

--- flask_app/controllers/contacts.py
+++ flask_app/controllers/contacts.py
@@ -5,3 +5,84 @@ from flask_app.models.user import User
 
 
 #ROUTES
+@app.route("/create")
+def new_contact():
+    if "user_id" not in session:
+        return redirect("/logout")
+    data = {
+        "id":session["user_id"]
+    }
+    return render_template("create_contact.html",user=User.get_by_id(data))
+
+
+@app.route("/create/contact",methods=["POST"])
+def create_contact():
+    if "user_id" not in session:
+        return redirect("/logout")
+    
+    if not Contact.validate_contact(request.form):
+        return redirect("/create")
+    data = {
+        "name": request.form["name"],
+        "phone_number": request.form["phone_number"],
+        "city": request.form["city"],
+        "state": request.form["state"],
+        # "vip": request.form["vip"],
+        "user_id": session["user_id"]
+    }
+    Contact.save(data)
+    return redirect("/dashboard")
+
+@app.route("/mycontacts/<int:id>")
+def my_contacts(id):
+    if "user_id" not in session:
+        return redirect("/logout")
+    data = {
+        "id": session["user_id"]
+    }
+    user_data = {
+        "id":id
+    }
+    return render_template("dashboard.html",user=User.get_by_id(user_data),all_contacts=Contact.get_all())
+
+
+@app.route("/edit/<int:id>")
+def edit_contact(id):
+    if "user_id" not in session:
+        return redirect("/logout")
+    data = {
+        "id":id
+    }
+    user_data = {
+        "id":session["user_id"]
+    }
+    return render_template("edit_contact.html",edit=Contact.get_one(data),user=User.get_by_id(user_data))
+
+
+@app.route("/update/contact",methods=["POST"])
+def update_contact():
+    if "user_id" not in session:
+        return redirect("/logout")
+    if not Contact.validate_contact(request.form):
+        (f"/edit/{request.form['id']}")
+    data = {
+        "name": request.form["name"],
+        "phone_number": request.form["phone_number"],
+        "city": request.form["city"],
+        "state": request.form["state"],
+        # "vip": request.form["vip"],
+        "id": request.form["id"]
+    }
+    Contact.update(data)
+    return redirect("/dashboard")
+
+
+@app.route("/destroy/contact/<int:id>")
+def destroy_contact(id):
+    if "user_id" not in session:
+        return redirect("/logout")
+    data = {
+        "id":id
+    }
+    Contact.destroy(data)
+    return redirect("/dashboard")
--- flask_app/models/contact.py
+++ flask_app/models/contact.py
@@ -1,3 +1,66 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app.models import user
+
+class Contact:
+    db = "dolphin_schema"
+    def __init__(self,db_data):
+        self.id = db_data["id"]
+        self.name = db_data["name"]
+        self.phone_number = db_data["phone_number"]
+        self.city = db_data["city"]
+        self.state = db_data["state"]
+        # self.vip = db_data["vip"]
+        self.user_id = db_data["user_id"]
+        self.created_at = db_data["created_at"]
+        self.updated_at = db_data["updated_at"]
+        self.user = None
+
+
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO contacts (name, phone_number, city, state, user_id) VALUES (%(name)s,%(phone_number)s,%(city)s,%(state)s,%(user_id)s);"
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM contacts;"
+        results =  connectToMySQL(cls.db).query_db(query)
+        all_contacts = []
+        for row in results:
+            all_contacts.append( cls(row) )
+        return all_contacts
+
+
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * FROM contacts WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return cls( results[0] )
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE contacts SET name=%(name)s, phone_number=%(phone_number)s, city=%(city)s, state=%(state)s, updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL(cls.db).query_db(query,data)
+        
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM contacts WHERE id = %(id)s;"
+        return connectToMySQL(cls.db).query_db(query,data)
+
+    @staticmethod
+    def validate_contact(contact):
+        is_valid = True
+        if len(contact['name']) < 2:
+            is_valid = False
+            flash("Contact name must be at least 2 characters","contact")
+        if len(contact['phone_number']) < 10:
+            is_valid = False
+            flash("Phone number must be at least 10 digits","contact")
+        if len(contact['city']) < 2:
+            is_valid = False
+            flash("City must be at least 2 characters", "contact")
+        if len(contact['state']) <2:
+            is_valid = False
+            flash("State must be at least 2 characters", "contact")
+        return is_valid
